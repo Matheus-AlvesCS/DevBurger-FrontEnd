d426369a947d5878741f5b5fa1cedc143557edd3
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2025-04-12 22:45:08 -0300

    Layout de usuário feito e otimização de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
+import { BrowserRouter } from "react-router";
 import { Elements } from "@stripe/react-stripe-js";
 
 import { ThemeProvider } from "styled-components";
 import { ToastContainer } from "react-toastify";
 import GlobalStyles from "./styles/globalStyles";
 
-import routes from "./routes";
 import { standardTheme } from "./styles/themes/standard";
 import AppProvider from "./hooks";
 import stripePromise from "./config/stripeConfig";
+import Router from "./routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -19,7 +19,9 @@ createRoot(document.getElementById("root")).render(
         <GlobalStyles />
         <ToastContainer autoClose={2000} theme="dark" pauseOnHover={false} />
         <Elements stripe={stripePromise}>
-          <RouterProvider router={routes} />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
         </Elements>
       </AppProvider>
     </ThemeProvider>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { Route, Routes } from "react-router";
 
 import {
   Login,
@@ -9,53 +9,23 @@ import {
   Checkout,
   CompletePayment,
 } from "../containers";
-import { Header, Footer } from "../components";
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Home />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/cadastro",
-    element: <Register />,
-  },
-  {
-    path: "/cardapio",
-    element: (
-      <>
-        <Header />
-        <Menu />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/carrinho",
-    element: (
-      <>
-        <Cart />
-      </>
-    ),
-  },
-  {
-    path: "/checkout",
-    element: <Checkout />,
-  },
-  {
-    path: "/complete",
-    element: <CompletePayment />,
-  },
-]);
+import { UserLayout } from "../layouts/UserLayout";
 
-export default routes;
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cardapio" element={<Menu />} />
+        <Route path="/carrinho" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/complete" element={<CompletePayment />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+    </Routes>
+  );
+}
+
+export default Router;
